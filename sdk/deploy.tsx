d0b4f19ac820981017fedd4fb87f1436d60b9f49
TraveWise <wise0shark@gmail.com>
2024-04-27 08:09:37 -0400

    sora

--- sdk/deploy.tsx
+++ sdk/deploy.tsx
@@ -10,6 +10,7 @@ async function deploySRC20Tokens(connection: Connection, payer: PublicKey, mintA
 	);
  
 	// Create a new token account
+	
 	const tokenAccount = await Token.createAccount(connection, mint, payer, payer);
  
 	console.log("Deployed SRC-20 tokens. Mint:", mint.publicKey.toBase58(), "Token Account:", tokenAccount.publicKey.toBase58());
